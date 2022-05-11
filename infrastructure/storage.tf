933ad6f9fb775dba5889726a85edbfb35c0cb7fb
FeGonca <felip.goncalves@hotmail.com>
2022-05-11 00:43:41 -0300

    Criações dos storages

---
+++ infrastructure/storage.tf
@@ -0,0 +1,6 @@
+resource "aws_s3_bucket" "buckets" {
+  count  = length(var.bucket_names)
+  bucket = "${local.prefix}-${var.bucket_names[count.index]}-${var.account}"
+
+  tags = local.common_tags
+}
