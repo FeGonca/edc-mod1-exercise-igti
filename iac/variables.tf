ffc4455d1cc326306cf74291e97f221e5219f36d
FeGonca <felip.goncalves@hotmail.com>
2022-05-10 23:51:15 -0300

    First Commit

---
+++ iac/variables.tf
@@ -0,0 +1,15 @@
+variable "base_bucket_name" {
+  default = "datalake-igti-tf"
+}
+
+variable "ambiente" {
+  default = "PRODUCAO"
+}
+
+variable "numero_conta" {
+  default = 123
+}
+
+variable "region" {
+  default = "sa-east-1"
+}
