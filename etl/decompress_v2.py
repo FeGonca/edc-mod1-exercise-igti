bbe3b1c5dcee5739720ce5fd624dc7daaed2c9af
FeGonca <felip.goncalves@hotmail.com>
2022-05-11 20:40:57 -0300

    Inclusão dos arquivos no S3

---
+++ etl/decompress_v2.py
@@ -0,0 +1,27 @@
+from urllib.request import urlretrieve
+import py7zr
+import os
+
+basepath = "./data"
+dlpath = f"{basepath}/rais"
+
+lista_path = os.listdir(dlpath)
+
+def decompress(arquivo):
+    filename = dlpath + '/' + arquivo    
+    print(filename)
+    archive = py7zr.SevenZipFile(filename)
+    archive.extractall(path=dlpath)
+    archive.close()
+    os.remove(filename)
+    return True
+    
+if __name__ == "__main__":
+    os.makedirs(dlpath, exist_ok=True)
+
+    for i in range(len(lista_path)):
+        print(f"Extracting from {lista_path[i]}")
+        res = decompress(lista_path[i])
+        print(res)
+    
+    print("Done!")
