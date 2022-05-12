f4d52b1878ebff52b19605f538c5245acddf5475
FeGonca <felip.goncalves@hotmail.com>
2022-05-11 21:54:53 -0300

    Inclusão dos arquivos no S3

--- infrastructure/storage_file.tf
+++ infrastructure/storage_file.tf
@@ -1,47 +1,47 @@
-resource "aws_s3_bucket_object" "r_co" {
-    bucket = "igti-felipe-rais-default-landing-zone-539475468352"
-    key = "rais/RAIS_VINC_PUB_CENTRO_OESTE.txt"
-    acl = "private"
-    source = "./data/rais/RAIS_VINC_PUB_CENTRO_OESTE.txt"
-    etag = filemd5("./data/rais/RAIS_VINC_PUB_CENTRO_OESTE.txt")
-}
+# resource "aws_s3_bucket_object" "r_co" {
+#     bucket = "igti-felipe-rais-default-landing-zone-539475468352"
+#     key = "rais/RAIS_VINC_PUB_CENTRO_OESTE.txt"
+#     acl = "private"
+#     source = "./data/rais/RAIS_VINC_PUB_CENTRO_OESTE.txt"
+#     etag = filemd5("./data/rais/RAIS_VINC_PUB_CENTRO_OESTE.txt")
+# }
 
-resource "aws_s3_bucket_object" "r_mg" {
-    bucket = "igti-felipe-rais-default-landing-zone-539475468352"
-    key = "rais/RAIS_VINC_PUB_MG_ES_RJ.txt"
-    acl = "private"
-    source = "./data/rais/RAIS_VINC_PUB_MG_ES_RJ.txt"
-    etag = filemd5("./data/rais/RAIS_VINC_PUB_MG_ES_RJ.txt")
-}
+# resource "aws_s3_bucket_object" "r_mg" {
+#     bucket = "igti-felipe-rais-default-landing-zone-539475468352"
+#     key = "rais/RAIS_VINC_PUB_MG_ES_RJ.txt"
+#     acl = "private"
+#     source = "./data/rais/RAIS_VINC_PUB_MG_ES_RJ.txt"
+#     etag = filemd5("./data/rais/RAIS_VINC_PUB_MG_ES_RJ.txt")
+# }
 
-resource "aws_s3_bucket_object" "r_no" {
-    bucket = "igti-felipe-rais-default-landing-zone-539475468352"
-    key = "rais/RAIS_VINC_PUB_NORDESTE.txt"
-    acl = "private"
-    source = "./data/rais/RAIS_VINC_PUB_NORDESTE.txt"
-    etag = filemd5("./data/rais/RAIS_VINC_PUB_NORDESTE.txt")
-}
+# resource "aws_s3_bucket_object" "r_no" {
+#     bucket = "igti-felipe-rais-default-landing-zone-539475468352"
+#     key = "rais/RAIS_VINC_PUB_NORDESTE.txt"
+#     acl = "private"
+#     source = "./data/rais/RAIS_VINC_PUB_NORDESTE.txt"
+#     etag = filemd5("./data/rais/RAIS_VINC_PUB_NORDESTE.txt")
+# }
 
-resource "aws_s3_bucket_object" "r_n" {
-    bucket = "igti-felipe-rais-default-landing-zone-539475468352"
-    key = "rais/RAIS_VINC_PUB_NORTE.txt"
-    acl = "private"
-    source = "./data/rais/RAIS_VINC_PUB_NORTE.txt"
-    etag = filemd5("./data/rais/RAIS_VINC_PUB_NORTE.txt")
-}
+# resource "aws_s3_bucket_object" "r_n" {
+#     bucket = "igti-felipe-rais-default-landing-zone-539475468352"
+#     key = "rais/RAIS_VINC_PUB_NORTE.txt"
+#     acl = "private"
+#     source = "./data/rais/RAIS_VINC_PUB_NORTE.txt"
+#     etag = filemd5("./data/rais/RAIS_VINC_PUB_NORTE.txt")
+# }
 
-resource "aws_s3_bucket_object" "r_sp" {
-    bucket = "igti-felipe-rais-default-landing-zone-539475468352"
-    key = "rais/RAIS_VINC_PUB_SP.txt"
-    acl = "private"
-    source = "./data/rais/RAIS_VINC_PUB_SP.txt"
-    etag = filemd5("./data/rais/RAIS_VINC_PUB_SP.txt")
-}
+# resource "aws_s3_bucket_object" "r_sp" {
+#     bucket = "igti-felipe-rais-default-landing-zone-539475468352"
+#     key = "rais/RAIS_VINC_PUB_SP.txt"
+#     acl = "private"
+#     source = "./data/rais/RAIS_VINC_PUB_SP.txt"
+#     etag = filemd5("./data/rais/RAIS_VINC_PUB_SP.txt")
+# }
 
-resource "aws_s3_bucket_object" "r_sul" {
-    bucket = "igti-felipe-rais-default-landing-zone-539475468352"
-    key = "rais/RAIS_VINC_PUB_SUL.txt"
-    acl = "private"
-    source = "./data/rais/RAIS_VINC_PUB_SUL.txt"
-    etag = filemd5("./data/rais/RAIS_VINC_PUB_SUL.txt")
-}
+# resource "aws_s3_bucket_object" "r_sul" {
+#     bucket = "igti-felipe-rais-default-landing-zone-539475468352"
+#     key = "rais/RAIS_VINC_PUB_SUL.txt"
+#     acl = "private"
+#     source = "./data/rais/RAIS_VINC_PUB_SUL.txt"
+#     etag = filemd5("./data/rais/RAIS_VINC_PUB_SUL.txt")
+# }
